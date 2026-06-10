9b047f7cab67701b66ec64ea15c88f94525e76ee
Anthony Haughton <anthony.haughton@peraton.com>
2026-06-10 10:43:42 -0400

    terra fmt

--- main.tf
+++ main.tf
@@ -9,21 +9,20 @@ resource "aws_vpc" "compute_vpc" {
 }
 
 resource "aws_subnet" "pub_subnet" {
-  count = length(local.target_zones)
-  vpc_id = aws_vpc.compute_vpc.id
-  cidr_block = "10.0.${count.index}.0/24"
+  count             = length(local.target_zones)
+  vpc_id            = aws_vpc.compute_vpc.id
+  cidr_block        = "10.0.${count.index}.0/24"
   availability_zone = local.target_zones[count.index]
 }
 
 resource "aws_subnet" "priv_subnet" {
-  count = length(local.target_zones)
-  vpc_id = aws_vpc.compute_vpc.id
+  count      = length(local.target_zones)
+  vpc_id     = aws_vpc.compute_vpc.id
   cidr_block = "10.0.${count.index + 10}.0/24"
 }
 
 resource "aws_internet_gateway" "compute_igw" {
   vpc_id = aws_vpc.compute_vpc.id
-  
 }
 
 resource "aws_route_table" "compute-public-rt" {
@@ -31,14 +30,14 @@ resource "aws_route_table" "compute-public-rt" {
 }
 
 resource "aws_route" "compute-public-route" {
-  route_table_id = aws_route_table.compute-public-rt.id
+  route_table_id         = aws_route_table.compute-public-rt.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.compute_igw.id
+  gateway_id             = aws_internet_gateway.compute_igw.id
 }
 
 resource "aws_route_table_association" "compute-rta" {
-  count = length(local.target_zones)
-  subnet_id = aws_subnet.pub_subnet[count.index].id
+  count          = length(local.target_zones)
+  subnet_id      = aws_subnet.pub_subnet[count.index].id
   route_table_id = aws_route_table.compute-public-rt.id
 }
 
@@ -72,26 +71,26 @@ resource "aws_security_group" "compute_ec2_sg" {
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_lb_to_ec2" {
-  security_group_id = aws_security_group.compute_ec2_sg.id
+  security_group_id            = aws_security_group.compute_ec2_sg.id
   referenced_security_group_id = aws_security_group.compute_lb_sg.id
-  from_port = 80
-  to_port = 80
-  ip_protocol = "tcp"
+  from_port                    = 80
+  to_port                      = 80
+  ip_protocol                  = "tcp"
 }
 
 resource "aws_vpc_security_group_egress_rule" "ec2_egress_rule" {
   security_group_id = aws_security_group.compute_ec2_sg.id
-  cidr_ipv4 = "0.0.0.0/0"
-  ip_protocol = "-1" # Needed so EC2 can pull packages during bootstrap
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # Needed so EC2 can pull packages during bootstrap
 }
 
 ## -- Load Balacing & Compute Block
 
 resource "aws_lb" "compute_lb" {
-  name = "compute-lb"
+  name               = "compute-lb"
   load_balancer_type = "application"
-  security_groups = [aws_security_group.compute_lb_sg.id]
-  subnets = [for subnet in aws_subnet.pub_subnet : subnet.id] 
+  security_groups    = [aws_security_group.compute_lb_sg.id]
+  subnets            = [for subnet in aws_subnet.pub_subnet : subnet.id]
 }
 
 resource "aws_lb_target_group" "compute-tg" {
@@ -123,27 +122,27 @@ resource "aws_lb_listener" "compute_listener" {
 }
 
 resource "aws_launch_template" "compute_launch_template" {
-  name = "compute-lt"
+  name          = "compute-lt"
   instance_type = "t2.micro"
-  image_id = "ami-0152204c1a187337c"
-  user_data = filebase64("${path.module}/userdata.sh")
+  image_id      = "ami-0152204c1a187337c"
+  user_data     = filebase64("${path.module}/userdata.sh")
 
   network_interfaces {
     associate_public_ip_address = false
-    security_groups = [aws_security_group.compute_ec2_sg.id]
+    security_groups             = [aws_security_group.compute_ec2_sg.id]
   }
 }
 
 resource "aws_autoscaling_group" "compute_asg" {
-  name = "compute_asg"
-  desired_capacity = 2
-  min_size = 2
-  max_size = 4
+  name                = "compute_asg"
+  desired_capacity    = 2
+  min_size            = 2
+  max_size            = 4
   vpc_zone_identifier = aws_subnet.pub_subnet[*].id
-  target_group_arns = [aws_lb_target_group.compute-tg.arn]
+  target_group_arns   = [aws_lb_target_group.compute-tg.arn]
 
   launch_template {
-    id = aws_launch_template.compute_launch_template.id
+    id      = aws_launch_template.compute_launch_template.id
     version = "$Latest"
   }
 }
--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "alb_dns_name" {
   description = "DNS name for ALB"
-  value = aws_lb.compute_lb.dns_name
+  value       = aws_lb.compute_lb.dns_name
 }
