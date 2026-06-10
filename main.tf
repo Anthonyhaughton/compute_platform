9b2f765d21602e1dd420bfd21cddc4a1fec8cf9e
Anthony <a@wgu.edu>
2026-06-10 19:21:42 -0400

    not needed

--- main.tf
+++ main.tf
@@ -78,19 +78,6 @@ resource "aws_vpc_security_group_ingress_rule" "allow_lb_to_ec2" {
   ip_protocol                  = "tcp"
 }
 
-resource "aws_security_group" "compute_ec2_ssh_sg" {
-  name        = "allow-ssh"
-  description = "allow SSH"
-  vpc_id      = aws_vpc.compute_vpc.id
-}
-
-resource "aws_vpc_security_group_ingress_rule" "compute-ssh" {
-  security_group_id = aws_security_group.compute_ec2_ssh_sg.id
-  from_port         = 22
-  to_port           = 22
-  ip_protocol       = "tcp"
-  cidr_ipv4         = "0.0.0.0/0"
-}
 resource "aws_vpc_security_group_egress_rule" "ec2_egress_rule" {
   security_group_id = aws_security_group.compute_ec2_sg.id
   cidr_ipv4         = "0.0.0.0/0"
